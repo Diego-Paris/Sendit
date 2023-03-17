428008c1505aa557166e891ec6c0a498f3546860
Diego-Paris <diego.paris@upr.edu>
2023-03-17 16:58:57 -0400

    Added Create Post Form

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,15 +1,17 @@
 import styles from './index.module.css';
-import { Button } from "@chakra-ui/react";
-import HeroSection from '../components/HeroSection'
+import { Button } from '@chakra-ui/react';
+import HeroSection from '../components/HeroSection';
 import Features from '../components/Features';
 import Testimonials from '../components/Testimonials';
+import CreatePostForm from '../components/CreatePost';
 
 export function Index() {
   return (
     <>
-      <HeroSection/>
-      <Features/>
-      <Testimonials/>
+      <CreatePostForm />
+      <HeroSection />
+      <Features />
+      <Testimonials />
     </>
   );
 }
